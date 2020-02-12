33958fb43ab25c502cc62759230d3dbd02a26a63
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-12 03:43:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+Class Dog 
+@@all = [] 
+def initialize(name) 
+  @name = name 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+ end 
+
+def self.clear_all 
+  @@all = [] 
+end 
+
+def self.print_all
+  @@all.each do | dog | 
+  puts "#{dog}"
+ end 
+end 
+
+def save 
+@@all << self 
+end 
+
+def name 
+  @name 
+  
+end 
+
+end 
